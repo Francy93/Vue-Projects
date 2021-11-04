d19f5921fe45f5aef0fbcb2671e1a319846dbae5
Francy <fa1033@live.mdx.ac.uk>
2021-11-04 22:40:25 +0000

    all cards loaded

---
+++ src/products.js
@@ -0,0 +1,91 @@
+// JSON objects of products
+
+const maths = {
+    "id":"3377",
+    "title":"Maths",
+    "location": "London",
+    "price":100,
+    "image":"assets/math-card.jpg",
+    "spaces":10
+}
+
+const english = {
+    "id":"3378",
+    "title":"English",
+    "location": "Manchester",
+    "price":90,
+    "image":"assets/english-card.png",
+    "spaces":9
+}
+
+const computerScience = {
+    "id":"3379",
+    "title":"CS",
+    "location": "LiverPool",
+    "price":130,
+    "image":"assets/computer-card.jpg",
+    "spaces":8
+}
+
+const music = {
+    "id":"3380",
+    "title":"Music",
+    "location": "Nottingam",
+    "price":80,
+    "image":"assets/music-card.jpg",
+    "spaces":7
+}
+
+const geography = {
+    "id":"3381",
+    "title":"Geography",
+    "location": "Cambridge",
+    "price":80,
+    "image":"assets/geography-card.png",
+    "spaces":6
+}
+
+const astronomy = {
+    "id":"3382",
+    "title":"Astronomy",
+    "location": "Brighton",
+    "price":70,
+    "image":"assets/astronomy-card.png",
+    "spaces":5
+}
+
+const geometry = {
+    "id":"3383",
+    "title":"Geometry",
+    "location": "Kent",
+    "price":90,
+    "image":"assets/geometry-card.jpg",
+    "spaces":11
+}
+
+const chemistry = {
+    "id":"3384",
+    "title":"Chemistry",
+    "location": "Oxford",
+    "price":115,
+    "image":"assets/chemistry-card.jpg",
+    "spaces":12
+}
+
+const biology = {
+    "id":"3385",
+    "title":"Biology",
+    "location": "Luton",
+    "price":110,
+    "image":"assets/biology-card.jpg",
+    "spaces":13
+}
+
+const arts = {
+    "id":"3386",
+    "title":"Arts",
+    "location": "Japan",
+    "price":150,
+    "image":"assets/art-card.jpg",
+    "spaces":14
+}
--- src/vue.js
+++ src/vue.js
@@ -1,35 +1,54 @@
+
 var vueApp = new Vue({
     el: '#app',
     data: {
-        sitename: 'After School Club',
+        // array of JSON objects
+        products: [arts, astronomy, biology, chemistry, computerScience, english, geography, geometry, maths, music],
         cart:[],
-        product: {
-            scope: this,
-            id: 3377,
-            title: 'Maths',
-            location: 'London',
-            price: 'Price: £100',
-            image: "./assets/math.png",
-            btn: 'Add To Cart',
-            spaces: 5,
-
-            addToCart: function(){
-                const cart = this.scope.vueApp.cart;
-                cart.push(this.id);
-                this.spaces--;
-            },
-        }
+        sitename: 'After School Club',
     },
     methods:{
-        AddToCart: function(){ this.product.addToCart(); }
+
+        validInput: function(elem){
+            if(typeof(elem) == "object" || typeof(elem) == "number"){
+                 if(typeof(elem) == "object") return 1;
+                else if(typeof(elem) == "number") return 2;
+            }else return 0;
+            return 0;
+        },
+
+        getProduct: function(elem){
+
+            switch(this.validInput(elem)){
+                case 1: return elem; // object case
+                case 2: return this.products[elem]; // number case
+                default: console.log("Wrong getProduct() parameter!");
+                    return false; // wrong value case
+            }
+        },
+
+        addToCart: function(elem){
+            const product = this.getProduct(elem);
+
+            if(product){
+                product.spaces--;
+                this.cart.push(product.id);
+                return true;
+            }else return false;
+        },
+
+        canAddToCart: function(elem){
+            const product = this.getProduct(elem);
+
+            if(product) return product.spaces > 0;
+            else false;
+        }
     },
 
     computed:{
+
         cartItemCount: function(){
-            return this.cart.length || '';
+            return this.cart.length;
         },
-        canAddToCart: function(){
-            return this.product.spaces > 0;
-        } 
     }
 });
